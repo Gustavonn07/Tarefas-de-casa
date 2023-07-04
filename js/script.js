c4028de64122c08e0e198f94b7aa05ecb005cd87
Gustavonn07 <gustavonepnog@gmail.com>
2023-07-03 21:12:54 -0300

    adicionando arquivos

--- js/script.js
+++ js/script.js
@@ -11,4 +11,44 @@ overlay.onclick = function() {
 fecharForm.onclick = function() {
     overlay.classList.remove('active');
     modal.classList.remove('active');
-}
+};
+
+let equipes = [];
+
+function listarEquipes() {
+    listaDeEquipes.innerHTML = '';
+    for(let i = 0; i < equipes.length; i++) {
+        listaDeEquipes.innerHTML += `
+            <li>
+                <h4>${equipes[i].nome}</h4>
+                <div>
+                    <h2>${equipes[i].participante} <span>/ ${equipes[i].qtdMax}</span> </h2>
+                    <button>
+                        <box-icon name="plus"></box-icon>Adicionar
+                    </button>
+                </div>
+            </li>
+        `;
+    }
+};
+
+function criarEquipe() {
+    // A linha de baixo serve para não usar o padrão do formulário (action):
+    event.preventDefault();
+
+    let equipeNome = document.querySelector('#equipe-nome');
+    let equipeQtd = document.querySelector('#equipe-qtd');
+
+    equipes.push(
+        {
+            nome: equipeNome.value,
+            qtdMax: equipeQtd.value,
+            participante: 0
+        },
+    );
+    
+    overlay.classList.remove('active');
+    modal.classList.remove('active');
+
+    listarEquipes();
+};
