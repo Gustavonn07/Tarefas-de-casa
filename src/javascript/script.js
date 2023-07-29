2c8d7ccc5a63d07d7ceb38773004192e246ba02d
Gustavonn07 <gustavonepnog@gmail.com>
2023-07-29 17:25:26 -0300

    finalizado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,18 +4,21 @@ const placeholder = document.getElementById('placeholder');
 const img = document.getElementById('img');
 const arrowUp = document.getElementById('up');
 const arrowDown = document.getElementById('down');
+const modal = document.getElementById('modal');
+const erro = document.getElementById('erro');
+const x = document.getElementById('x');
+let arrayTentativas = [];
 let tentativas = 0;
 
 function geradorDeNumero() {
     return numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-}
+};
 
 const numFinal = geradorDeNumero();
 
-tentativasInput.addEventListener('keydown', function(event) {
+tentativasInput.addEventListener('keyup', function(event) {
 
     if(event.key === 'Enter') {
-        setasAuxiliadoras();
         
         if(tentativasInput.value == numFinal) {
             tentativas = 0;
@@ -33,65 +36,128 @@ tentativasInput.addEventListener('keydown', function(event) {
 
         } else {
             tentativas++;
+            arrayTentativas.push(tentativasInput.value);
 
-            if(tentativas === 1) {
-                if(tentativasInput.value == '') {
-                    tentativasSPAN.textContent += `0`;
-
-                } else {
-                    tentativasSPAN.textContent += `${tentativasInput.value}`;
-                    
-                }
-
-            } else {
-                if(tentativasInput.value == '') {
-                    tentativasSPAN.textContent += `- 0 `;
-
-                } else {
-                    tentativasSPAN.textContent += ` - ${tentativasInput.value}`;
-                    
-                }
-
+            if(arrayTentativas.includes('')) {
+                arrayTentativas.splice(arrayTentativas.indexOf(''), 1, '0');
             }
+            
+            if(!erroModal(tentativasInput.value, arrayTentativas)) {
+            
+                setasAuxiliadoras();
 
-            tentativasInput.value = ''
+                if(tentativas === 1) {
+                    if(tentativasInput.value == '') {
+                        tentativasSPAN.textContent += `0`;
+    
+                    } else {
 
-            if(tentativas === 10) {
-                img.src = `src/images/Derrota.png`;
+                        if(arrayTentativas[tentativas - 1] === undefined) {
+                            tentativasSPAN.textContent += ``;
 
-                placeholder.innerHTML = `
-                <p class="main__paragraph main__paragraph--textLoser">
-                Você não acertou, o número era: <span id="numeroFinal" class="main__span">${numFinal}</span>
-                </p>
+                        } else {
+                            tentativasSPAN.textContent += `${arrayTentativas[tentativas - 1]}`;
 
-                <button class="main__btn" id="btn">Tentar novamente</button>
-                `;
-                
-                const btn = document.getElementById('btn');
-                btn.addEventListener('click', () => {
-                    location.reload();
-                });
-
-                tentativasInput.style.display= 'none';
-                arrowUp.style.display = 'none';
-                arrowDown.style.display = 'none';
+                        };
+                    };
+    
+                } else {
+                    if(arrayTentativas[tentativas - 1] === undefined) {
+                        tentativasSPAN.textContent += ``;
+                        
+                    } else {
+                        tentativasSPAN.textContent += ` - ${arrayTentativas[tentativas - 1]}`;
+                        
+                    };
+                };
+    
+                tentativasInput.value = '';
+    
+                if(tentativas === 10) {
+                    img.src = `src/images/Derrota.png`;
+    
+                    placeholder.innerHTML = `
+                    <p class="main__paragraph main__paragraph--textLoser">
+                    Você não acertou, o número era: <span id="numeroFinal" class="main__span">${numFinal}</span>
+                    </p>
+    
+                    <button class="main__btn" id="btn">Tentar novamente</button>
+                    `;
+                    
+                    const btn = document.getElementById('btn');
+                    btn.addEventListener('click', () => {
+                        location.reload();
+                    });
+    
+                    tentativasInput.style.display= 'none';
+                    arrowUp.style.display = 'none';
+                    arrowDown.style.display = 'none';
+                };
             };
         };
     };
 });
 
 function setasAuxiliadoras() {
-    if(tentativasInput.value > numFinal ) {
+    const ultimaTentativa = Number(arrayTentativas[arrayTentativas.length - 1]);
+
+    if (ultimaTentativa > numFinal) {
         arrowUp.classList.remove('main__arrow--active');
         arrowDown.classList.add('main__arrow--active');
 
-    } else if(tentativasInput.value < numFinal) {
+    } else if (ultimaTentativa < numFinal) {
         arrowUp.classList.add('main__arrow--active');
         arrowDown.classList.remove('main__arrow--active');
 
     } else {
         arrowUp.classList.remove('main__arrow--active');
         arrowDown.classList.remove('main__arrow--active');
-        
+
     }
-}
+};
+
+function erroModal(numeroTeste, array) {
+    if(possuiValorMaiorQue100(numeroTeste, array)) {
+        modal.style.display = 'flex';
+        erro.textContent = 'Valor menor do que 100, tente novamente!';
+        arrayTentativas.pop()
+
+    } else if(possuiValorMenorQue0(numeroTeste, array)) {
+        modal.style.display = 'flex';
+        erro.textContent = 'Valor menor do que 0, tente novamente!';
+        arrayTentativas.pop()
+
+    } else if (possuiValoresRepetidos(array)) {
+        modal.style.display = 'flex';
+        erro.textContent = 'Valor repetido, tente novamente!';
+
+    };
+};
+
+function possuiValorMaiorQue100(numeroTeste) {
+    return numeroTeste > 100;
+};
+
+function possuiValorMenorQue0(numeroTeste) {
+    return numeroTeste < 0;
+};
+
+function possuiValoresRepetidos(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+          if (array[i] === array[j]) {
+            array.pop();
+            return true;
+          };
+        };
+      };
+
+    return false;
+};
+
+x.addEventListener('click', () => {
+    modal.style.display = 'none';
+    erro.textContent = '';
+
+    tentativas--;
+});
