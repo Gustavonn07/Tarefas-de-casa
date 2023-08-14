1cba90dbc8c7ca2985f816efab820d754476d462
Gustavonn07 <gustavonepnog@gmail.com>
2023-08-14 11:01:53 -0300

    darkmode

--- src/components/Darkmode/index.tsx
+++ src/components/Darkmode/index.tsx
@@ -8,13 +8,25 @@ const Darkmode = () => {
     const trocarModo = () => {
         return setClicks(clicks + 1);
     }
+
+    const trocarCor = () => {
+        if (clicks % 2 === 0) {
+            document.body.classList.remove('dark');
+            document.body.classList.add('light');
+        } else {
+            document.body.classList.add('dark');
+            document.body.classList.remove('light');
+        }
+    }
+
+    trocarCor();
     
     return (
         <>
             <button className='darkmode__btn' onClick={trocarModo}>
                 <img
                     className='darkmode__icon'
-                    src={clicks % 2 === 0 ? "images/sun.svg" : "images/moon.svg"} 
+                    src={clicks % 2 === 0 ? "images/sun.svg" : "images/moon.svg"}
                     alt={clicks % 2 === 0 ? "Imagem de um sol" : "Imagem de uma lua"}
                  />
             </button>
--- src/components/GridPrincipal/index.tsx
+++ src/components/GridPrincipal/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import './style.css'
+import './style.css';
 
 type PropsGridPrincipal  = {
     children: ReactNode;
